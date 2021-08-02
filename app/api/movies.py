a745dc8fc97460be715a6e0ecc6950bf9b6d4d69
Jay <74105438+weijay0804@users.noreply.github.com>
2021-08-02 21:13:10 +0800

    更改目綠結構 -> movies_datas

--- app/api/movies.py
+++ app/api/movies.py
@@ -16,13 +16,16 @@ from ..models import Movies
 def get_movies():
     movie_id = request.args.get('id')
     movie_limit = request.args.get('limit')
+
     if movie_id:
         movie = Movies.query.get_or_404(movie_id)
         print(movie_id)
         return jsonify({'movie' : movie.to_json()})
+
     elif movie_limit:
         movies = Movies.query.limit(movie_limit).all()
         return jsonify({'movies' : [movie.to_json() for movie in movies]})
+
     else:
         movies = Movies.query.all()
         print(movie_id)
@@ -39,6 +42,7 @@ def get_hot_movies():
 def get_top_movies():
     movies = Movies.query.filter((Movies.source == 'top') | (Movies.source == 'top_hot')).order_by(Movies.rate.desc()).all()
     return jsonify({'movies' : [movie.to_json() for movie in movies]})
+
 
 
     
--- app/static/js/top_movies.js
+++ app/static/js/top_movies.js
@@ -40,8 +40,6 @@ function print(dataset) {
 
         }
 
-        
-
 
         newCard.className = 'infoCard'
 
