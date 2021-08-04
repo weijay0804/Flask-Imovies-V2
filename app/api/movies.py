2e3f4c8e4347fdde5750ad9c1755f7a64eee0fc6
Jay <74105438+weijay0804@users.noreply.github.com>
2021-08-04 12:51:34 +0800

    修改 movies api

--- app/api/movies.py
+++ app/api/movies.py
@@ -1,48 +1,59 @@
 '''
 
-    movies API
+    movies 的資源端點
 
 '''
 
-
-from flask import jsonify, request
-from sqlalchemy.orm import query
-from app.models import Movies
-from . import api
 from ..models import Movies
+from flask import jsonify,request
+from . import api
 
 
 @api.route('/movies/')
 def get_movies():
-    movie_id = request.args.get('id')
-    movie_limit = request.args.get('limit')
+    ''' 取得所有電影 '''
 
-    if movie_id:
-        movie = Movies.query.get_or_404(movie_id)
-        print(movie_id)
-        return jsonify({'movie' : movie.to_json()})
+    if request.args.get('limit'):
 
-    elif movie_limit:
-        movies = Movies.query.limit(movie_limit).all()
-        return jsonify({'movies' : [movie.to_json() for movie in movies]})
-
+        movies = Movies.query.limit(request.args.get('limit')).all()
     else:
+
         movies = Movies.query.all()
-        print(movie_id)
-        return jsonify({'movies' : [movie.to_json() for movie in movies]})
-    
-    
-@api.route('/hot_movies')
-def get_hot_movies():
-    movies = Movies.query.filter((Movies.source == 'hot') | (Movies.source == 'top_hot')).all()
     return jsonify({'movies' : [movie.to_json() for movie in movies]})
 
 
-@api.route('/top_movies')
+@api.route('/movies/<int:id>/')
+def get_movie(id):
+    ''' 依照 movie id 取得電影'''
+    movie = Movies.query.get_or_404(id)
+    return jsonify(movie.to_json())
+
+
+@api.route('/trend_movies/')
+def get_trend_movies():
+    ''' 取得熱門電影 '''
+
+    if request.args.get('limit'):
+        movies = Movies.query.filter(( Movies.source == 'hot' ) | ( Movies.source == 'top_hot' )).limit(request.args.get('limit')).all()
+    else:
+        movies = Movies.query.filter(( Movies.source == 'hot' ) | ( Movies.source == 'top_hot' )).all()
+    
+    return jsonify({'movies' : [movie.to_json() for movie in movies]})
+
+
+@api.route('/top_movies/')
 def get_top_movies():
-    movies = Movies.query.filter((Movies.source == 'top') | (Movies.source == 'top_hot')).order_by(Movies.rate.desc()).all()
+    ''' 取得 TOP 250 電影 '''
+
+    if request.args.get('limit'):
+        movies = Movies.query.filter(( Movies.source == 'top' ) | ( Movies.source == 'top_hot') ).limit(request.args.get('limit')).all()
+    
+    else:
+        movies = Movies.query.filter(( Movies.source == 'top') | ( Movies.source == 'top_hot' )).all()
     return jsonify({'movies' : [movie.to_json() for movie in movies]})
 
 
 
-    
+
+
+
