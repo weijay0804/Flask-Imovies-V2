4b90932a23813472de1fdfb9ef34052076de624e
Jay <74105438+weijay0804@users.noreply.github.com>
2021-08-20 11:08:28 +0800

    新增 imovies.deploy 函式

--- imovies.py
+++ imovies.py
@@ -5,8 +5,7 @@
 '''
 
 import os
-from flask_login.config import USE_SESSION_FOR_NEXT
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 
 #----自訂函式----
 from app import create_app, db
@@ -25,4 +24,17 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@app.cli.command()
+def deploy():
+    ''' 在部屬前設定資料庫 '''
+    # 更新資料庫
+    upgrade()
+
+    # 更新資料庫電影
+    Movies.insert_movies_datas('hot')
+    Movies.insert_movies_datas('top')
+
+    
+
 
